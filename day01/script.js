02098661f028d3706510d080e553bcaa4c873fc2
hyorim72 <suny153086@gmail.com>
2026-08-21 17:34:13 +0900

    feat: 01. 영화 데이터 변수 만들기

---
+++ day01/script.js
@@ -0,0 +1,11 @@
+// 과제1.영화 데이터 변수 만들기
+const title = "인터스텔라";
+const voteAverage = 8.7;
+const voteCount = 32000;
+const popularity = 150.5;
+const releaseDate = "2014-11-05";
+const originalLanguage = "en"
+
+// 과제2.영화 테이터 Console출력하기
+
+//과제 3.
