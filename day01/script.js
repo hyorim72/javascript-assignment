b0f2bba30acdb535388d9b418264711837a64dfa
hyorim72 <suny153086@gmail.com>
2026-08-21 17:57:00 +0900

    feat: 03. Number 데이터 연산하기

--- day01/script.js
+++ day01/script.js
@@ -4,8 +4,17 @@ const voteAverage = 8.7;
 const voteCount = 32000;
 const popularity = 150.5;
 const releaseDate = "2014-11-05";
-const originalLanguage = "en"
+const originalLanguage = "en";
 
 // 과제2.영화 테이터 Console출력하기
+console.log(title);
+console.log(voteAverage);
+console.log(voteCount);
+console.log(popularity);
+console.log(releaseDate);
+console.log(originalLanguage);
 
-//과제 3.
+//과제 3.Number 데이터 연산하기
+const additionalVoteCount = 100;
+console.log(voteCount+additionalVoteCount);
+
