4342013e959df406a0aec3c33541e1ef33788ade
hyorim72 <suny153086@gmail.com>
2026-08-26 02:45:30 +0900

    feat: 02. 영화 정보 함수 만들기

---
+++ day03/script.js
@@ -0,0 +1,10 @@
+// 2.영화 정보 함수 만들기
+
+const getMovieMessage = (title, voteAverage) => {
+  return ` ${title}의 평점은 ${voteAverage}점입니다.`;
+};
+
+// 3. 함수의 반환값 사용하기
+const message = getMovieMessage("인셉션", 8.4);
+console.log(message);
+
