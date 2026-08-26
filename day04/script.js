36f2d65f1ee24312c95df704e05c31aa33c73ec5
hyorim72 <suny153086@gmail.com>
2026-08-26 18:18:37 +0900

    feat: 02. 버튼의 Click Event 처리하기

--- day04/script.js
+++ day04/script.js
@@ -1,3 +1,8 @@
 // 2. 버튼의 Click Event 처리하기
 
+const recommendbutton = document.querySelector("#recommend-button");
+const recommendresult = document.querySelector("#recommend-result");
 
+recommendButton.addEventListtener("click", () => {
+  recommendResult.textContent = "오늘의 추천 영화는 인셉션입니다.";
+});
