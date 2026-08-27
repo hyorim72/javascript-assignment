adedbf2c631827b7b0fd7c4efd5c6ae8fc344268
hyorim72 <suny153086@gmail.com>
2026-08-27 17:30:05 +0900

    과제 3: filter()로 평점이 높은 영화 찾기

--- day05/script.js
+++ day05/script.js
@@ -22,3 +22,13 @@ const movies = [
 const foundMovie = movies.find((movie) => movie.title === "인터스텔라");
 console.log(`${foundMovie.title}의 평점은 ${foundMovie.voteAverage}점입니다.`);
 
+// 3. filter()로 평점이 높은 영화 찾기
+
+const filteredMovies = movies.filter((movie) => {
+  return movie.voteAverage >= 8.5;
+});
+
+console.log(filteredMovies);
+// movies는 여러 영화가 들어가있는 복수형아다. 꺼내온 1개의 영화 평점은 8.5이상인지 검사한다.
+
+
