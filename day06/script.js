9be0eb883715b5aacedbf5d16aa90a26a555fd34
hyorim72 <suny153086@gmail.com>
2026-08-28 15:47:51 +0900

    feat: JSON.parse()를 이용한 JavaScript 값으로 변환하기

--- day06/script.js
+++ day06/script.js
@@ -14,3 +14,9 @@ console.log(movie);
 const movieJson = JSON.stringify(movie);
 console.log(movieJson);
 console.log(typeof movieJson);
+
+// 04. JSON 문자열을 JavaScript 값으로 변환하기
+
+const paraedMovie = JSON.parse(movieJson);
+console.log(paraedMovie);
+console.log(typeof paraedMovie);
