93499dd8bc01fcd0812d6edcb1fe06d2a77882d8
hyorim72 <suny153086@gmail.com>
2026-08-31 16:18:14 +0900

    feat: 06번 ||와 ?? 연산자 비교

--- day07/script.js
+++ day07/script.js
@@ -37,4 +37,8 @@ console.log(director);
 const directorName = director ?? "감독 정보 없음";
 console.log(directorName);
 
+// 06.  ||와 ??의 차이 확인하기
 
+const { vote_count } = movie;
+console.log(vote_count || 100);
+console.log(vote_count ?? 100);
