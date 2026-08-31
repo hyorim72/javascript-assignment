e3d7d999c3f7ecf102835eb4bb3e0f5835bd2258
hyorim72 <suny153086@gmail.com>
2026-08-31 15:51:12 +0900

    feat: 02번 새로운 변수 이름 할당

--- day07/script.js
+++ day07/script.js
@@ -1,8 +1,22 @@
+const movie = {
+  id: 550,
+  title: "Fight Club",
+  vote_average: 8.4,
+  vote_count: 0,
+  detail: {
+    runtime: 139,
+  },
+};
+
+const genres = ["Drama", "Thriller", "Crime"];
 
 // 01. 객체 구조 분해 할당하기
 
-const { title , vote_average} = movie;
+const { title, vote_average } = movie;
+console.log(`제목: ${title}`);
+console.log(`평점: ${vote_average}`);
 
+// 02. 새로운 변수 이름으로 할당하기
 
-
-
+const { title: movieTitle } = movie;
+console.log(movieTitle);
